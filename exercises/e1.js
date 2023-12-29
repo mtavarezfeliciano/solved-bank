c9a9f3e0d27515aebb3a56240b5391295a626362
Michael Tavarez-Feliciano <mtavarezfeliciano@gmail.com>
2023-12-29 14:54:10 -0500

    lvl1 done

--- exercises/e1.js
+++ exercises/e1.js
@@ -4,9 +4,18 @@
 // getFirstTenNumbers() => [1, 2, 3, ... , 10]
 
 export function getFirstTenNumbers() {
-  // Your code goes here...
+  let newArr = [];
+  for (let i = 1; i <= 10; i++) {
+    newArr.push(i);
+  }
+  return newArr;
+} 
 
-}
+let finalProduct = [];
+finalProduct.push(getFirstTenNumbers());
+
+console.log(finalProduct);
+
 
 // === TEST YOURSELF ===
 // Once you're finished run the test with "npm run test-1"
