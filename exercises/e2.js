052b0335e8c11780116d84581dc8437906d05fbf
Michael Tavarez-Feliciano <mtavarezfeliciano@gmail.com>
2023-12-29 18:20:04 -0500

    lvl 2 done

--- exercises/e2.js
+++ exercises/e2.js
@@ -2,10 +2,22 @@
 // for every number between 0 and max find all numbers that have square roots that are integers
 // getNumbersWithSquareRoots(17) => [0, 1, 4, 9, 16]
 
-export function getNumbersWithSquareRoots(max) {
-  // Your code goes here...
+//every number needs to be checked 
+//if it checks out a number that has a sqrt, then push it 
 
+export function getNumbersWithSquareRoots(max) {
+  let sqrArr = [];
+  for (let i = 0; i < max; i++) {
+    if ((Math.sqrt(i) % 1) == 0) {
+      sqrArr.push(i);
+    }
+  }
+  return sqrArr;
 }
+let finalArr = [];
+finalArr.push(getNumbersWithSquareRoots(17));
+
+console.log(finalArr);
 
 // === TEST YOURSELF ===
 // Once you're finished run the test with "npm run test-2"
